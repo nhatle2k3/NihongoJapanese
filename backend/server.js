923b0627247feb9f4d28fa35d5190577298b7247
Nhat Lee <levannhat01222459426@gmail.com>
2026-06-30 20:23:51 +0700

    Configure Render frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -38,11 +38,3 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-app.use(cors({
-  origin: [
-    "https://nihongo-frontend.onrender.com",
-    "http://localhost:5173"
-  ],
-  credentials: true
-}));
