d9b85a43b483247a5d2e256d96e0297e3bf632d0
Nhat Lee <levannhat01222459426@gmail.com>
2026-07-01 09:22:16 +0700

    Prepare project for Render deploy

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,12 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-
-const connectionString = process.env.DATABASE_URL;
+const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-  connectionString,
-  ssl: process.env.NODE_ENV === 'production'
-    ? { rejectUnauthorized: false }
-    : false,
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 module.exports = {
