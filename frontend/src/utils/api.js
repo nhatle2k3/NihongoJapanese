303c1c46b7f3684fe996efd97861f590e3a28d62
Nhat Lee <levannhat01222459426@gmail.com>
2026-07-01 09:06:56 +0700

    Prepare project for Render deploy

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,6 @@
-const BASE_URL = `${import.meta.env.VITE_API_URL || 'http://localhost:5000'}/api`;
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
+
+export default BASE_URL;
 
 export const getAuthToken = () => localStorage.getItem('token');
 export const setAuthToken = (token) => {
